56a5669b36388d1e7d13ffbceb45acd808e9f997
陈不进 <69799234+PastGone@users.noreply.github.com>
2024-07-07 04:25:36 +0800

    My feature5 (#4)
    * 添加素材。
    
    * 主要包括以下修改。
    1.添加分离和合并的逻辑。
    2.添加对树木和房屋的碰撞检测。
    3.将显示的倍数和实际倍数区分开来。用数字标识。
    4.添加一个主界面，游戏结束会自动进入主界面。结束条件是当就人物数值是0.2时
    5.调整了角色的碰撞区域

---
+++ other_snowball.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+@export var is_editor:bool
+@onready var shape_cast_2d: ShapeCast2D = $ShapeCast2D
+
+
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+
+	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	
+	if body.is_in_group("gold"):
+		print(body)
+		
+		body.queue_free()
+	elif body.is_in_group("pitfall"):
+		print(body)
+		self.queue_free()
+		body.queue_free()
+		
+	pass # Replace with function body.
--- player.tres.gd
+++ player.tres.gd
@@ -4,31 +4,44 @@ extends CharacterBody2D
 
 const SPEED = 600.0
 const JUMP_VELOCITY = -800.0
+@onready var getitem: AudioStreamPlayer = $"../sound_manger/getitem"
+@onready var jump: AudioStreamPlayer = $"../sound_manger/jump"
+@onready var crash_s: AudioStreamPlayer = $"../sound_manger/crash_s"
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
+
+var happy:Texture=preload("res://assets/textures/tset/雪球_开心.PNG")
+var sad:Texture=preload("res://assets/textures/tset/雪球_伤心.PNG")
+
+var other:=preload("res://build/other_snowball.tscn")
+#子节点等于数量。 
+var other_index:int=1
+
+var add_other_num=1
+
+
+var max_scale:=Vector2(4,4)
+var scale_num:float=1
+var display_current_scale=Vector2(1,1)
+
+@onready var label: Label = $Label
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	if scale>max_scale:
+		scale=max_scale
+
+	scale_num=display_current_scale.x/Vector2(1,1).x
+	if scale_num<0.5:
+		label.self_modulate=Color(255,0,0,255)
+	if scale_num<0.2:
+		get_tree().change_scene_to_packed(load("res://main_interface.tscn"))
+	label.text=str("X","%.1f" % scale_num)
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		pass
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	if Input.is_action_just_pressed("ui_accept") and shape_cast_2d.is_colliding()  :
-
-		velocity.y = JUMP_VELOCITY
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if is_editor:
-		velocity.x = SPEED
+	velocity.x = SPEED
 	move_and_slide()
 	
 	
@@ -36,18 +49,85 @@ func _input(event:InputEvent):
 	if event is InputEventMouseButton and shape_cast_2d.is_colliding():
 		if event.button_index==MOUSE_BUTTON_LEFT and event.is_pressed() :
 			velocity.y = JUMP_VELOCITY
+			jump.play()
 			return
-					#清除路标
-		
-	pass
+					
+					
+					
+	
+#	合并的逻辑在这里
+	if Input.is_action_just_pressed("ui_accept") and shape_cast_2d.is_colliding()  :
+		for c in self.get_children():
+			if c.is_in_group("snow_ball"):
+				c.queue_free()
+				
+			pass
+		pass
+		display_current_scale=self.scale*other_index  
+		# 缩放不能超过最大值
+		if self.scale*other_index >max_scale:
+			self.scale=max_scale
+		else:
+			self.scale*=other_index
+		# 重置其他数量
+		other_index=1
+		add_other_num=1
+		return
 
+#	分裂的逻辑在这里。 
+	if event is InputEventMouseButton and self.scale>Vector2(2,2) :
+		if event.button_index==MOUSE_BUTTON_RIGHT and event.is_pressed() :
+			self.scale/=2
+			display_current_scale/=2
+			for i in range(0,add_other_num):
+				var a:CharacterBody2D=other.instantiate()
+				a.position.x=other_index*-200
+				other_index+=1
+				self.add_child(a)
+				add_other_num*=2
+				pass
+		pass
+	
+	
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	
 	if body.is_in_group("gold"):
 		print(body)
+		getitem.play()
+		sprite_2d.texture=happy
+		if scale<Vector2(10,10):
+			#self.scale+=Vector2(1,1)
+			self.scale+=Vector2(0.05/other_index,0.05/other_index)
+			display_current_scale+=Vector2(0.05/other_index,0.05/other_index)
+		
 		body.queue_free()
+	
+	
+	
+	elif body.is_in_group("tree")and scale>Vector2(2.5,2.5):
+		crash_s.play()
+		sprite_2d.texture=happy
+		self.scale+=Vector2(0.1/other_index,0.1/other_index)
+		display_current_scale+=Vector2(0.1/other_index,0.1/other_index)
+		body.q_f()
+		
+	elif body.is_in_group("house") :
+		if scale<Vector2(2.5,2.5):
+			body.open_door()
+		else:
+			crash_s.play()
+			sprite_2d.texture=happy
+			self.scale+=Vector2(0.2/other_index,0.2/other_index)
+			display_current_scale+=Vector2(0.2/other_index,0.2/other_index)
+			body.q_f()
+
+#减的逻辑 
 	elif body.is_in_group("pitfall"):
 		print(body)
+		crash_s.play()
+		sprite_2d.texture=sad
+		self.scale-=Vector2(0.1/other_index,0.1/other_index)
+		display_current_scale-=Vector2(0.1/other_index,0.1/other_index)
 		body.queue_free()
 	pass # Replace with function body.
